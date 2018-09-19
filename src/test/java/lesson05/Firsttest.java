687c1a3b221b26ac38a4015c76d5777652626e59
nynatalka <natachak2007@gmail.com>
2018-09-19 19:39:53 +0300

    qwerty

---
+++ src/test/java/lesson05/Firsttest.java
@@ -0,0 +1,37 @@
+package lesson05;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class Firsttest {
+    static WebDriver wd;
+
+    @BeforeClass
+    public static void setUp() {
+        wd=new ChromeDriver();
+        wd.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+         wd.get("http://automationpractice.com/index.php?id_category=8&controller=category");
+         wd.manage().window();
+    }
+
+@AfterClass
+public static void tearDown(){
+    wd.quit();
+}
+@Test
+public void verifyFirstSerchResult() {
+    wd.findElement(By.id("search_query_top")).click();
+    wd.findElement(By.id("search_query_top")).sendKeys("Dress");
+    wd.findElement(By.id("search_query_top")).submit();
+    Assert.assertThat(wd.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a"))
+            .getText(), containsString("Dress"));
+
+}
+}
